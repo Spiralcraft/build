3b7c2c01136df9d39acdbe27acc8db9cff74443f
Mike Toth <mike.git@spiralcraft.com>
2020-12-12 20:13:19 -0800

    Add shell scripts for build

---
+++ bin/common.sh
@@ -0,0 +1,13 @@
+export SANDBOX="$(realpath $(dirname $0)/../../..)"
+if [ -f "$(dirname $0)/.sandbox.env" ]; then
+  source $(dirname $0)/.sandbox.env
+fi
+export SCBUILD=$SANDBOX/build
+if [ -f "$SANDBOX/build.local/gitbash.env" ]; then
+  source $SANDBOX/build.local/gitbash.env
+fi
+export ANTJAR="$SCBUILD/lib/ant-launcher.jar"
+export JAVA=$JAVA_HOME/java
+if [ -f "$(dirname $0)/.env" ]; then
+  source $(dirname $0)/.env
+fi
